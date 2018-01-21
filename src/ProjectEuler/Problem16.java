537831f66374754430a79a49509549287bb76d67
jian <1978048527@qq.com>
2018-01-21 18:40:10 +0800

    problem 16

---
+++ src/ProjectEuler/Problem16.java
@@ -0,0 +1,40 @@
+package ProjectEuler;
+
+import java.math.BigDecimal;
+
+public class Problem16 {
+
+    public static void main(String[] args) {
+        long no = 1000;
+        String res = fun(no);
+        System.out.println("计算结果:"+res);
+        System.out.println(sum(res));
+    }
+
+    public static String fun(long no){
+        BigDecimal mul = new BigDecimal("32768");
+        for(int i=15;i<no;i++){
+            mul = mul.multiply(new BigDecimal("2"));
+        }
+        return mul.toString();
+    }
+
+    public static long sum(String res){
+        long sum = 0;
+        if(res.length() > 0){
+            for (int i=0;i<res.length();i++){
+                if(Integer.valueOf(res.charAt(i)+"") >= 0 ){
+
+                    sum += Integer.valueOf(res.charAt(i)+"");
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void ts(){
+        System.out.println("nothing");
+
+    }
+
+}
